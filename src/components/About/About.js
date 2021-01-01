01da228e0249705098488b9bc8549c615144e971
pokimochi <nolan0597@gmail.com>
2021-01-01 17:43:24 -0500

    navbar changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./About.css"
-import headshot from "../../resources/images/about/headshot.png";
+import headshot from "../../resources/images/about/headshot-3.png";
 
 const About = () => {
   return (
@@ -11,8 +11,8 @@ const About = () => {
         I'm Patricia Alyson T. Deogracias (or Pat for short), and I am a student in <b>Computer Animation at Ringling College of Art and Design</b>. 
         <br />
         <br />
-        My focus is on <b>illustration, animation, and visual development</b>! I am especially passionate about representation in media, and creating art that explores the idea of home, and identity.
-        Aside from drawing, I love mornings, random animal facts, horror!
+        My focus is on <b>storyboarding, with an interest in animation</b>! I am especially passionate when it comes to representation in media, and creating art that explores the idea of home, and identity.
+        Aside from drawing, I love mornings, random animal facts and horror!
         <br />
         <br />
         Feel free to reach out to my <a id="email-link" href="mailto: patriciatdeo@gmail.com">e-mail</a>, or social media!
